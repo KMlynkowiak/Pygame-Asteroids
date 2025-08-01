8f9869e250563eff55263f874cc9300e9d932282
Kacper Mlynkowiak <Kmlynkowiak@edu.cdv.pl>
2025-08-01 17:04:57 +0200

    small adjustments 2

--- circleshape.py
+++ circleshape.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 # Base class for game objects
 class CircleShape(pygame.sprite.Sprite):
     def __init__(self, x, y, radius):
@@ -7,13 +8,14 @@ class CircleShape(pygame.sprite.Sprite):
             super().__init__(self.containers)
         else:
             super().__init__()
-
         self.position = pygame.Vector2(x, y)
         self.velocity = pygame.Vector2(0, 0)
         self.radius = radius
 
     def draw(self, screen):
-        pass  # to be overridden by subclasses
+        # must override
+        pass
 
     def update(self, dt):
-        pass  # to be overridden by subclasses
+        # must override
+        pass
--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
+from constants import *
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 
 
 class Player(CircleShape):
@@ -8,28 +8,24 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
 
-    def triangle(self):
-        forward = pygame.Vector2(0, -1).rotate(self.rotation)
-        right = pygame.Vector2(2, 0).rotate(self.rotation + 90) * self.radius / 1.5
-
-        a = self.position + forward * self.radius
-        b = self.position - forward * self.radius + right
-        c = self.position - forward * self.radius - right
-
-        return [a.xy, b.xy, c.xy]
-
-
-        return [a, b, c]
-
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
-    def rotate(self, dt):
-        self.rotation += PLAYER_TURN_SPEED * dt
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
