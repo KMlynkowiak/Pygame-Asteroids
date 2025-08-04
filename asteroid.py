f842e0d00781c721a9286af4a7844ae1f2125ace
Kacper Mlynkowiak <Kmlynkowiak@edu.cdv.pl>
2025-08-04 19:56:38 +0200

    Asteroid Split

--- asteroid.py
+++ asteroid.py
@@ -1,8 +1,11 @@
 import pygame
+import random
+from constants import ASTEROID_MIN_RADIUS
 from circleshape import CircleShape
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
+        pygame.sprite.Sprite.__init__(self, self.containers)
         super().__init__(x, y, radius)
 
     def draw(self, screen):
@@ -10,3 +13,23 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return  # najmniejsze asteroidy nie dzielą się dalej
+
+        angle = random.uniform(20, 50)
+        v1 = self.velocity.rotate(angle) * 1.2
+        v2 = self.velocity.rotate(-angle) * 1.2
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        from asteroid import Asteroid  # opóźniony import, unik cyklicznych zależności
+
+        a1 = Asteroid(self.position.x, self.position.y, new_radius)
+        a1.velocity = v1
+
+        a2 = Asteroid(self.position.x, self.position.y, new_radius)
+        a2.velocity = v2
